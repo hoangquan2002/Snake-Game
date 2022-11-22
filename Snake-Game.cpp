26576a6fd12fadb6389ad25bb41a41dd061d3d59
UIT-21521486 <21521486@gm.uit.edu.vn>
2022-11-22 23:29:19 +0700

    MENU

--- Snake-Game.cpp
+++ Snake-Game.cpp
@@ -18,3 +18,81 @@ using namespace std;
 #define HEIGHT 20
 #define BODY '*'
 #define APPLE 'O'
+
+// Show menu at the end of the game
+
+void showEndMenu()
+{
+	system("cls");
+	gotoxy(0, 0);
+	cout << "End game!" << endl;
+	cout << "Your score: " << score << endl;
+	cout << "Do you want to play again ([y]/[n]): ";
+	char option;
+	cin >> option;
+	option = tolower(option);
+	if (option == 'y')
+	{
+		resetSnake();
+		startGame();
+	}
+	else if (option == 'n')
+		exit(1);
+}
+
+void startGame()
+{
+	system("cls");
+	ShowConsoleCursor(false);
+
+	drawBox();
+	drawSnake();
+	genApple();
+	displayScore();
+
+	while (true)
+	{
+		if (_kbhit())
+		{
+			char ch = _getch();
+			ch = tolower(ch);
+			if (ch == 'a' && direction != Direction::right)
+				direction = Direction::left;
+			else if (ch == 'w' && direction != Direction::down)
+				direction = Direction::up;
+			else if (ch == 's' && direction != Direction::up)
+				direction = Direction::down;
+			else if (ch == 'd' && direction != Direction::left)
+				direction = Direction::right;
+			else if (ch == 'q') // Quit game
+			{
+				showEndMenu();
+				break;
+			}
+		}
+		move();
+		drawHeadnTail();
+		if (isAteApple())
+		{
+			score++;
+			displayScore();
+			growing();
+			genApple();
+		}
+		if (isBiteItself())
+		{
+			ShowConsoleCursor(true);
+			showEndMenu();
+			break;
+		}
+		if (isHitWall())
+		{
+			ShowConsoleCursor(true);
+			showEndMenu();
+			break;
+		}
+		Sleep(speed);
+	}
+}
+
+
