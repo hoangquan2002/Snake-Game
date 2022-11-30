11190095cdbe5f64faac6cb14561a55ed4f560b7
UIT-20521943 <20521943@gm.uit.edu.vn>
2022-11-30 14:37:30 +0700

    upload

--- Snake-Game/Snake-Game/Snake-Game/Snake-Game.cpp
+++ Snake-Game/Snake-Game/Snake-Game/Snake-Game.cpp
@@ -1,47 +1,26 @@
-#include <iostream>
-using namespace std;
-
-/*
- * Import library
- */
-#include <vector>
-#include <windows.h>
-#include <conio.h>
-#include <cstring>
-#include <time.h>
-#include <random>
-
-//cccccccccccccccccccccccc
-
- /*
-  * MACRO
-  */
-#define WIDTH 40
-#define HEIGHT 20
-#define BODY '*'
-#define APPLE 'O'
-
-  /*
-   * Enum
-   */
-/*
-khai báo hàm
-*/
-bool isBiteItself();
-/*
-phan cua nhat
-*/
-
-if (isBiteItself())
-			break;
-bool isBiteItself()
+// Generate apple on the board
+void genApple()
 {
-	Point head = snake[0];
-	for (size_t i = 1; i < snake.size(); i++)
-		if (head.x == snake[i].x && head.y == snake[i].y)
-			return true;
-	return false;
+	srand(time(0));
+	int x = rand() % (WIDTH - 1) + 1;
+	int y = rand() % (HEIGHT - 1) + 1;
+	apple = {
+		x,
+		y,
+	};
+	gotoxy(x, y);
+	cout << APPLE;
 }
-/*hello
-*/
-// cout ran
+
+// Check if the snake ate apple
+bool isAteApple()
+{
+	return snake[0].x == apple.x && snake[0].y == apple.y;
+}
+
+// Show score on right side of the board
+void displayScore()
+{
+	gotoxy(WIDTH + 5, 2);
+	cout << "Your score: " << score;
+}
